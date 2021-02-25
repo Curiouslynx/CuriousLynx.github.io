ccfc277298056c51ebd2702372c0d8f0764718d6
CuriousLynx <box2288@yandex.ru>
2021-02-25 21:42:29 +0300

    redirect http: to https:

--- dist/https.js
+++ dist/https.js
@@ -1,18 +1,16 @@
 /*global window */
 /*jslint browser: true */
 
-// (function (root) {
-//   "use strict";
-//   var h = root ? root.location.hostname : "";
-//   var p = root ? root.location.protocol : "";
-//   if ("http:" === p && !(/^(localhost|127.0.0.1)/).test(h)) {
-//     root.location.protocol = "https:";
-//   }
-// }("undefined" !== typeof window ? window : this));
+(function (root) {
+  "use strict";
+  var h = root ? root.location.hostname : "";
+  var p = root ? root.location.protocol : "";
+  if ("http:" === p && !(/^(localhost|127.0.0.1)/).test(h)) {
+    root.location.protocol = "https:";
+  }
+}("undefined" !== typeof window ? window : this));
 
 
 // if (window.location.protocol != 'https:') location.href = location.href.replace("http://", "https://");
 
-if (window.location.protocol === "http:") {
-  window.location.protocol = "https:";
-}
+// if (window.location.protocol === "http:") window.location.protocol = "https:";
